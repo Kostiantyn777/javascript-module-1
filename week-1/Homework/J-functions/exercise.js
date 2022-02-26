9bd67b04d7b988cdf9debafed34ff1b21ab2644b
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-02-26 08:11:52 +0100

    Homework J-functions added

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
-var result = halve(12);
+let result1 = halve(12);
+let result2 = halve(14);
+let result3 = halve(16);
 
-console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,12 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
-var result = triple(12);
+let result1 = triple(12);
+let result2 = triple(13);
+let result3 = triple(14);
 
-console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
