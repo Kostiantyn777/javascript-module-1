7c62a96ecf191c1b18e1703e7ff83e4e8eb055ab
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-03-05 08:54:35 +0100

    Added G-conditionals-2 homework

--- week-2/Homework/G-conditionals-2/exercise-4.js
+++ week-2/Homework/G-conditionals-2/exercise-4.js
@@ -9,7 +9,12 @@
 */
 
 function containsCode(sentence) {
-
+  let wordCode = "code";
+  if (sentence.indexOf(wordCode) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -19,11 +24,11 @@ var sentence1 = "code your future";
 var sentence2 = "draw your future";
 var sentence3 = "design your future";
 
-console.log("'" + sentence1 + "': " + containsCode(sentence1))
-console.log("'" + sentence2 + "': " + containsCode(sentence2))
-console.log("'" + sentence3 + "': " + containsCode(sentence3))
+console.log("'" + sentence1 + "': " + containsCode(sentence1));
+console.log("'" + sentence2 + "': " + containsCode(sentence2));
+console.log("'" + sentence3 + "': " + containsCode(sentence3));
 
-    /* 
+/* 
     EXPECTED RESULT
     ---------------
     'code your future': true
