78c0d6e97995cf1dee408ab79cb6f2e9ce4f0b76
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-03-05 15:59:13 +0100

    Fixed exercise-J

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,10 @@
+const arrayOfNames = ["Bob", "Amy", "Alice"];
+
 function secondMatchesAmy(array) {
-  if (array[1]==='Amy' ) {
+  if (array[1] === "Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+
+console.log(secondMatchesAmy(arrayOfNames));
